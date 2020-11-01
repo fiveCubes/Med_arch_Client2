4e11e6f4a4a790141a8557df6787187982e462db
vishnu <vishnuvijayakumar125@gmail.com>
2020-11-01 13:53:04 +1100

    Updates

---
+++ precache-manifest.1e0c43ea817054ae34eb9a3b613ea3c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f5369a47524524e35ca714ef1b89b6",
+    "url": "/Med_arch_Client2/index.html"
+  },
+  {
+    "revision": "190886cbd91309c6d4a9",
+    "url": "/Med_arch_Client2/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "34152075ac005f5261d3",
+    "url": "/Med_arch_Client2/static/css/main.155939d3.chunk.css"
+  },
+  {
+    "revision": "190886cbd91309c6d4a9",
+    "url": "/Med_arch_Client2/static/js/2.4a910026.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Med_arch_Client2/static/js/2.4a910026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34152075ac005f5261d3",
+    "url": "/Med_arch_Client2/static/js/main.7c586e7e.chunk.js"
+  },
+  {
+    "revision": "ff615ae55d780e3049a0",
+    "url": "/Med_arch_Client2/static/js/runtime-main.65442d24.js"
+  },
+  {
+    "revision": "9d586c5e95deccaa9ccf91df641ff187",
+    "url": "/Med_arch_Client2/static/media/vase_logo.9d586c5e.png"
+  }
+]);
